583f3d0c1b6514795fa97de67e992e295d2604ac
Joao Esperancinha <jofisaes@gmail.com>
2022-07-02 23:36:05 +0200

    NGINX Improvements

---
+++ nginx/Readme.md
@@ -0,0 +1,5 @@
+# Healthy Cameras NGINX Configuration
+
+## References
+
+- [Prometheus Routing and Routing Configuration with Nginx Reverse Proxy by SOBYTE](https://www.sobyte.net/post/2022-03/prometheus-nginx-proxy/)
--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-sed -i 's/${PROMETHEUS}'"/${PROMETHEUS}/g" /etc/nginx/conf.d/default.conf
-sed -i 's/${GRAFANA}'"/${GRAFANA}/g" /etc/nginx/conf.d/default.conf
+sed -i 's/${PROMETHEUS}'"/${PROMETHEUS}/g" /etc/nginx/conf.d/prometheus.conf
+sed -i 's/${GRAFANA}'"/${GRAFANA}/g" /etc/nginx/conf.d/grafana.conf
 
-cat /etc/nginx/conf.d/default.conf
+cat /etc/nginx/conf.d/prometheus.conf
+cat /etc/nginx/conf.d/grafana.conf
 
 nginx -t
 

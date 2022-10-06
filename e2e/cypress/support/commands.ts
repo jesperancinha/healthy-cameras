da98c0bdb4f50e52083d85a5179b16a898936602
Joao Esperancinha <jofisaes@gmail.com>
2022-10-07 00:08:36 +0200

    Renaming and new documentation

--- e2e/cypress/support/commands.ts
+++ e2e/cypress/support/commands.ts
@@ -262,15 +262,15 @@ Cypress.Commands.add('logAll', () => {
 
 export function createCamera2HmacHeaders(method: string, path: string): Partial<any> {
     const username = 'cameraUser2', secret = 'dragon', algorithm = 'hmac-sha256';
-    const dateFormat = new Date().toUTCString();
+    const currentDateTimeUtc = new Date().toUTCString();
     const digestBodyHeader = `SHA-256=${crypto.createHash('sha256').digest('base64')}`;
-    const signingString = `x-date: ${dateFormat}\n${method} ${path} HTTP/1.1\ndigest: ${digestBodyHeader}`;
+    const signingString = `x-date: ${currentDateTimeUtc}\n${method} ${path} HTTP/1.1\ndigest: ${digestBodyHeader}`;
     const signature = crypto.createHmac('sha256', secret).update(signingString).digest('base64');
     const authorization = `hmac username="${username}", algorithm="${algorithm}", headers="x-date request-line digest", signature="${signature}"`;
     return {
         'Digest': digestBodyHeader,
         'Authorization': authorization,
-        'X-Date': dateFormat,
+        'X-Date': currentDateTimeUtc,
         'Content-Type': 'application/json',
     };
 }

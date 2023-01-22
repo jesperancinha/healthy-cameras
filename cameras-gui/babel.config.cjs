4da4c197c739bfa116b92000c1cf15461beef9d7
Joao Esperancinha <jofisaes@gmail.com>
2023-01-22 14:28:12 +0100

    Not yet

---
+++ cameras-gui/babel.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {
+      modules: "commonjs",
+    targets: {
+      node: 'current'
+      }}],
+    '@babel/preset-typescript',
+  ],
+};

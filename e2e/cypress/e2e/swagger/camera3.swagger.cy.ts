b7a73c7dbcda169937a5afc930225e1257e6e5bc
João Esperancinha <jofisaes@gmail.com>
2025-11-09 15:42:10 +0100

    Refactor test assertion to use `cy.contains` for better readability in all camera swagger test files

--- e2e/cypress/e2e/swagger/camera3.swagger.cy.ts
+++ e2e/cypress/e2e/swagger/camera3.swagger.cy.ts
@@ -9,7 +9,7 @@ describe('Swagger tests spec for Camera 3', () => {
     it('passes', () => {
         cy.get('input[class="download-url-input"]').clear().type(`${Cypress.config().baseUrl}${applicationRootCamera3}/v3/api-docs`);
         cy.get('button').contains('Explore').click();
-        cy.get('h2', {timeout: 10000}).contains('OpenAPI definition', {timeout: 10000}).should('not.be.null');
+        cy.contains('OpenAPI definition', {timeout: 10000}).should('not.be.null');
         cy.wait(1000);
 
         cy.get('div[class="servers"] > label > select > option').should('have.value', `http://localhost:8000${applicationRootCamera3}`);
